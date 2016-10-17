718844ca2c00d505578016ddb29424366134e64c
Leonel Oliveira <leoneloliver@gmail.com>
2016-10-17 11:42:32 -0400

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,3 +1,13 @@
+/* 
+ _                           _   _____ _ _           _           
+| |                         | | |  _  | (_)         (_)          
+| |     ___  ___  _ __   ___| | | | | | |___   _____ _ _ __ __ _ 
+| |    / _ \/ _ \| '_ \ / _ \ | | | | | | \ \ / / _ \ | '__/ _` |
+| |___|  __/ (_) | | | |  __/ | \ \_/ / | |\ V /  __/ | | | (_| |
+\_____/\___|\___/|_| |_|\___|_|  \___/|_|_| \_/ \___|_|_|  \__,_|
+                                                                 
+Front-end Developer: leoneloliver@gmail.com
+*/
 var App = React.createClass({
   getInitialState: function() {
     return {
@@ -39,3 +49,4 @@ var App = React.createClass({
   }
 });
 React.render(<App source="http://codepen.io/jobs.json" />, document.querySelector("#result"));
+
